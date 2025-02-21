e99413309f5e2a56746d60250711fbd8ff50f76a
Joel Kram <joelkram@Joels-MacBook-Air.local>
2025-02-21 12:42:26 +0000

    set up folders

--- lib/main.rb
+++ lib/main.rb
@@ -6,20 +6,3 @@
 #
 #lets start with modules/functional
 #
-def create_board()
-  board = []
-  row = ["   " "|" "   " "|" "   "]
-  seperator = "---------------"
-  board.push(row)
-  board.push(seperator)
-  board.push(row)
-  board.push(seperator)
-  board.push(row)
-  p board
- 
-end
-create_board()
-#print board to console with lines
-def print_board(create_board)
-  
-end
