9ca2890684195a4ccbd80527d243c96f6c223242
Joel Kram <joelnskra@gmail.com>
2025-02-21 15:25:27 +0000

    refracturing some classes

--- lib/main.rb
+++ lib/main.rb
@@ -33,3 +33,41 @@ end
 
 
 
+#refractor to classes
+
+class Board
+  def initialize(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    @board = board
+    
+  end
+
+  def print_board()
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} " 
+    puts "-----------" 
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} " 
+    puts "-----------" 
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} " 
+  end
+
+  def board_update(player_move)
+    @board[player_move] = "X"
+    print_board
+  end
+end
+
+class Player
+  def initialize(playerX)
+    @playerX = playerX
+    
+  end
+
+  def player_move()
+    puts "Enter a number between 1-9 to make your move"
+    move = gets.chomp
+    index = move.to_i - 1
+    return index
+  end
+end
+
+
+
