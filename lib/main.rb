62f4e8280b6e3a96af186f45469a6d87a106bd7a
Joel Kram <joelnskra@gmail.com>
2025-02-21 14:50:23 +0000

    trying different things

--- lib/main.rb
+++ lib/main.rb
@@ -8,28 +8,28 @@
 #
 
 #prints board to CL
-def display_board(board)
-  board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+def display_board()
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   puts " #{board[0]} | #{board[1]} | #{board[2]} " 
-  # puts "-----------" 
+  puts "-----------" 
   puts " #{board[3]} | #{board[4]} | #{board[5]} " 
-  # puts "-----------" 
+  puts "-----------" 
   puts " #{board[6]} | #{board[7]} | #{board[8]} " 
 
 end
 
-#helper method to convert input to index
-def user_input_to_index(user_input)
-  puts "Where do you want to play?" #won't need this when I can feed user input as argument
-  user_input = gets #won't need this when I can feed user input as argument
-  user_index = user_input.to_i - 1
-  return user_index
+def player_move()
+  puts "Enter a number between 1-9 to make your move"
+  move = gets.chomp
+  index = move.to_i - 1
+  return index
+end
 
+def make_move(index)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board[index] = "X"
+  return board
 end
-# user_input_to_index("")
-# 
-def move(board, user_index, who_is_playing)
-  board[user_index] = who_is_playing
-  board
-end
-display_board(0)
+
+
+
