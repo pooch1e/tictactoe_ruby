24c01cc20c3817043c54ed8e9f40513adf15149c
Joel Kram <joelnskra@gmail.com>
2025-02-23 09:12:13 +0000

    created player class

--- lib/main.rb
+++ lib/main.rb
@@ -43,15 +43,39 @@ class Board
 end
 
 class Player
-  def initialize(player, symbol)
-    @player = player
-    @symbol = symbol
+  def initialize() #x or o
   end
 
+  def who_is_playing?(symbol)
+    @symbol = symbol
+    symbol.upcase()
+    puts "#{symbol} is player 1, 0 is player 2"
+  end
+
+  def player_choice()
+    puts "Where do you want to play?"
+    input1 = gets.chomp
+    input2 = gets.chomp 
+    #how to output this for the gameboard
+    return input1, input2
+  end
 end
 
 
+
+
+  
+
 game = Board.new
+player1 = Player.new
+player1.who_is_playing?("x")
+
+player1.player_choice()
+update_board("x", input1, input2)
 game.print_board
+
+
+
+
 
 
