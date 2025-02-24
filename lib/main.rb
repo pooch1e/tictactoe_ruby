87f8fb3ef861b16993bdedaa1498eb04ddcb4a3c
Joel Kram <joelnskra@gmail.com>
2025-02-24 12:12:43 +0000

    added gameplay loop, fixed classes

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,5 @@
-#ruby tic tac toe
+#attempt to refracture this
+
 #what is the model?
 #build board (view)
 #build controller (players/turns)
@@ -23,7 +24,10 @@ class Board
   def update_board(player, x, y) #will be X or 0 for player, str input - x/y is coords for array
     if valid_move?(x, y) #check if space is empty
       @board[x][y] = player
+    else
+      puts "Already filled, try again"
     end
+  
   end
 
   def valid_move?(x, y)
@@ -31,40 +35,26 @@ class Board
 
   end
 
-  #attempt to refracture this
-  def winner? 
-    #check accross only
-    @board.each do |row|
-      if row.all? {|cell| cell == "X"}
-        puts "Winning Combo"
-        return true
-      else
-        puts "continue"
-        return false
-      end
-
-      #check down?
-      (0..2).each do |col|
-        if @board.all? { |row| row[col] == "X"}
-          puts "Winning Combo"
-          return true
-        else
-          puts "Continue"
-          return false
-        end
-      end
-
-      #check diagonal?
-      #check left to bottom right
-      if @board[0][0] == "X" && @board[1][1] == "X" && @board[2][2] == "X" || @board[0][2] == "X" && @board[1][1] == "X" && @board[2][0] == "X" #top right to bottom left
-        puts "Winning Combo"
-        return true
-      else
-        return false
-      end
-    end
+#new def winner?
+def winner?
+  # Check rows
+  @board.each do |row|
+    return true if row.uniq.length == 1 && row.first != " "
   end
+
+  # Check columns
+  (0..2).each do |col|
+    return true if @board.all? { |row| row[col] == @board[0][col] } && @board[0][col] != " "
+  end
+
+  # Check diagonals
+  return true if [@board[0][0], @board[1][1], @board[2][2]].uniq.length == 1 && @board[1][1] != " "
+  return true if [@board[0][2], @board[1][1], @board[2][0]].uniq.length == 1 && @board[1][1] != " "
+
+  false
 end
+end
+
 
 #Handle Player
 class Player
@@ -75,39 +65,44 @@ class Player
     puts "Who is playing?"
     symbol = gets.chomp
     symbol.to_s.upcase()
-    puts "#{symbol} is player 1, 0 is player 2"
+    puts "You are #{symbol}"
     return symbol
   end
 
   def player_choice()
-    puts "Where do you want to play?"
-    input1 = gets.chomp
-    input2 = gets.chomp 
-    #how to output this for the gameboard
-    return input1, input2
+    loop do
+      puts "Enter row (0-2):"
+      x = gets.chomp.to_i
+      puts "Enter column (0-2):"
+      y = gets.chomp.to_i
+      return x, y if x.between?(0, 2) && y.between?(0, 2)
+  
+      puts "Invalid input. Please enter numbers between 0 and 2."
+    end
   end
 end
+
 
 
 #Handle Game Logic
 class GamePlay
-  def initialize()
+  def initialize
     @turn = 0
-    @game_end = false
+    @x_turn = true
     puts "Begin the game!"
   end
 
-  def who_move()
+  def who_move
     puts "The turn is #{@turn}"
-    if @turn == 0
-      puts "X's move"
-      @turn += 1
-    elsif @turn % 2 == 1
-      puts "0's move"
-      @turn += 1
-    end
+    player = @x_turn ? "X" : "O"
+    puts "It's #{player}'s turn"
+    @x_turn = !@x_turn
+    @turn += 1
+    player
   end
 end
+
+
 
 
 
@@ -130,27 +125,21 @@ new_game = GamePlay.new
 # Start the game
 symbol = player1.who_is_playing
 
+
 # Game loop
-while new_game.instance_variable_get(:@turn) < 10 && !game.winner?
-  new_game.who_move
+while !game.winner? && new_game.instance_variable_get(:@turn) < 9
+  symbol = new_game.who_move
   game.print_board
 
-  input1, input2 = player1.player_choice
-  game.update_board(symbol, input1.to_i, input2.to_i)
-  if symbol == "X"
-    player1.who_is_playing == "0"
-  else
-    return symbol
+  x, y = player1.player_choice
+  until game.valid_move?(x, y)
+    puts "Invalid move, try again."
+    x, y = player1.player_choice
   end
-  game.print_board
+  game.update_board(symbol, x, y)
 
   break if game.winner?
 end
 
-puts "Game Over"
-
-
-
-
-
-
+puts "Game Over, #{symbol} won"
+game.print_board
